20ce78e165dbd4f0b768d9c85f4ad9d02fb91631
heimushareper <heimusharper@gmail.com>
2021-01-08 21:34:30 +0400

    license

--- libSGCS/connection/serial/SerialConnectionFabric.h
+++ libSGCS/connection/serial/SerialConnectionFabric.h
@@ -1,3 +1,19 @@
+/*
+ * This file is part of the SGCS distribution (https://github.com/heimusharper/SGCS).
+ * Copyright (c) 2020 Andrey Stepanov.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef SERIALCONNECTIONFABRIC_H
 #define SERIALCONNECTIONFABRIC_H
 #include <QAbstractListModel>
